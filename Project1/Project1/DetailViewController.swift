abf057e9fc7116128d3dfb05ef9ceee221c0423d
Frulwinn <frulwinn@gmail.com>
2019-10-31 14:42:52 -0500

    created Detail view controller on IB and swift file and classed it

---
+++ Project1/Project1/DetailViewController.swift
@@ -0,0 +1,32 @@
+//
+//  DetailViewController.swift
+//  Project1
+//
+//  Created by Lambda_School_Loaner_34 on 10/31/19.
+//  Copyright © 2019 Frulwinn. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
