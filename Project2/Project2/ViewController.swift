62d35fee9022de4e5d7b89e1b7d712979e52a5d2
Frulwinn <frulwinn@gmail.com>
2019-11-04 14:48:41 -0600

    added countries in array, set button images with function askQuestions

--- Project2/Project2/ViewController.swift
+++ Project2/Project2/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    //properties
+    var countries = [String]()
+    var score = 0
+    
+    
     //outlets
     @IBOutlet var button1: UIButton!
     @IBOutlet var button2: UIButton!
@@ -18,9 +23,17 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        askQuestion()
+        
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        
     }
 
-
+    func askQuestion() {
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+    }
 }
 
