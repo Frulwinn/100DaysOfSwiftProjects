130066099fa2bdc6a7ca16f97d37058b060d5e08
Frulwinn <frulwinn@gmail.com>
2019-11-20 22:41:57 -0600

    added observe value func to tell us which key path was changed

--- Project4/Project4/ViewController.swift
+++ Project4/Project4/ViewController.swift
@@ -44,6 +44,12 @@ class ViewController: UIViewController, WKNavigationDelegate {
         
     }
     
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "estimatedProgress" {
+            progressView.progress = Float(webView.estimatedProgress)
+        }
+    }
+    
     @objc func openTapped() {
         let ac = UIAlertController(title: "Open page...", message: nil, preferredStyle: .actionSheet)
         ac.addAction(UIAlertAction(title: "apple.com", style: .default, handler: openPage))
@@ -57,6 +63,7 @@ class ViewController: UIViewController, WKNavigationDelegate {
         let url = URL(string: "https://" + action.title!)!
         webView.load(URLRequest(url: url))
     }
+    
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         title = webView.title
     }
